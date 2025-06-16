3d7684510ef116a40fb25120a9bbea65ca158365
Leonardo-Davio <leonardo.davio@edu.unifi.it>
2025-06-16 16:01:15 +0200

    Creazione dei primi eventi dal portale admin, sistemato hompage.html

--- main/views.py
+++ main/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
+from .models import Event
+
 
 
 # Create your views here.
 
 def index(response):
-    return render(response, "main/homepage.html", {})
+    today = timezone.now()
+    print(today)
+    events = Event.objects.filter(date__gte=today)
+    print(events)
+    return render(response, "main/homepage.html", {"events": events})
