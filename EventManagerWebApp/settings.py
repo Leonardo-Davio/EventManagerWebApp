ac0b84f4caa34da0e64f700a6895a939a681830d
Leonardo-Davio <leonardo.davio@edu.unifi.it>
2025-06-28 20:48:13 +0200

    prep deploy

--- EventManagerWebApp/settings.py
+++ EventManagerWebApp/settings.py
@@ -82,18 +82,18 @@ WSGI_APPLICATION = 'EventManagerWebApp.wsgi.application'
 
 
 
-if os.getenv('DB_LIVE') in ['True', True]:
+if os.environ.get('DB_LIVE') in ['True', True]:
     DATABASES = { #public
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('DB_NAME'),
-            'USER': os.getenv('DB_USER'),
-            'PASSWORD': os.getenv('DB_PASSWORD'),
-            'HOST': os.getenv('DB_HOST'),
-            'PORT': os.getenv('DB_PORT'),
+            'NAME': os.environ.get('DB_NAME'),
+            'USER': os.environ.get('DB_USER'),
+            'PASSWORD': os.environ.get('DB_PASSWORD'),
+            'HOST': os.environ.get('DB_HOST'),
+            'PORT': os.environ.get('DB_PORT'),
         }
     }
-elif os.getenv('DB_LIVE') in ['True', True] and os.getenv('TYPE_DB') in ['Sql', True]:
+elif os.environ.get('DB_LIVE') in ['True', True] and os.environ.get('TYPE_DB') in ['Sql', True]:
     DATABASES = { #local sqlite
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
